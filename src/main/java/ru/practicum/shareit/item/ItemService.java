e969c0a8bd7e9ba2dcc809fe1a0441012e387ed5
Lisendor <Lisendor@mail.ru>
2025-10-31 01:22:01 +0700

    14 Спринт(Исправление Checkstyle)

--- src/main/java/ru/practicum/shareit/item/ItemService.java
+++ src/main/java/ru/practicum/shareit/item/ItemService.java
@@ -5,8 +5,12 @@ import java.util.List;
 
 public interface ItemService {
     ItemDto createItem(ItemDto itemDto, Long ownerId);
+
     ItemDto updateItem(Long itemId, ItemDto itemDto, Long ownerId);
+
     ItemDto getItem(Long itemId);
+
     List<ItemDto> getUserItems(Long ownerId);
+
     List<ItemDto> searchItems(String text);
 }
