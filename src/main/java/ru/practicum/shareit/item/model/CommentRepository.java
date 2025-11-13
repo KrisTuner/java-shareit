096e0c56784e251c42666c2fd7a45c449c54f211
Lisendor <Lisendor@mail.ru>
2025-11-13 20:52:23 +0700

    Спринт 15(Исправление Checkstyle)

--- src/main/java/ru/practicum/shareit/item/model/CommentRepository.java
+++ src/main/java/ru/practicum/shareit/item/model/CommentRepository.java
@@ -7,6 +7,7 @@ import java.util.List;
 @Repository
 public interface CommentRepository extends JpaRepository<Comment, Long> {
     List<Comment> findByItemId(Long itemId);
+
     List<Comment> findByItemIdIn(List<Long> itemIds);
 
     boolean existsByItemIdAndAuthorId(Long itemId, Long authorId);
